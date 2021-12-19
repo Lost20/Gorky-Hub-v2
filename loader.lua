243158409c737257af23970596416d7d3ab70453
Lost <82897733+Lost20@users.noreply.github.com>
2021-12-19 16:15:32 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 6803397337 then
-loadstring(game:HttpGet("hub.gorkys.xyz/avatarrater", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lost20/Gorky-Hub-v2/main/avatarrater.lua", true))()
 end 
  
 if game.PlaceId == 5965573089 then
-loadstring(game:HttpGet("hub.gorkys.xyz/ragdollking", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lost20/Gorky-Hub-v2/main/ragdollking.lua", true))()
 end
 
 if game.PlaceId == 6740271685 then

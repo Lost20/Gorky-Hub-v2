a0afe92aeb7c6f6de7ee54678b73aa40c27cd90d
Lost <82897733+Lost20@users.noreply.github.com>
2021-12-19 15:18:47 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 6803397337 then
+loadstring(game:HttpGet("hub.gorkys.xyz/avatarrater", true))()
+end 
+ 
+if game.PlaceId == 5965573089 then
+loadstring(game:HttpGet("hub.gorkys.xyz/ragdollking", true))()
+end
+
+if game.PlaceId == 6740271685 then
+loadstring(game:HttpGet("hub.gorkys.xyz/skateloader", true))()
+end
